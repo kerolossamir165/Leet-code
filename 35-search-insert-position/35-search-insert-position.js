6f1a167803a9f1f4a8fccfefc28e629804f2d0dd
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-10 19:25:04 +0200

    Time: 71 ms (82.39%), Space: 42.3 MB (10.20%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,11 +4,19 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (var i = 0; i < nums.length; i++) {
-         if (nums[i] >= target) {
-            return i 
-         }
-             
-     }
-  return nums.length;
+ let start = 0,
+    end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    }
+  }
+  return start;
 };
