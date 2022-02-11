6b25cfea2dd3d3efe75b1b238d82318173f88178
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-11 18:49:17 +0200

    Time: 72 ms (98.71%), Space: 43.6 MB (42.24%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,7 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  let sort = (str)=> str.split('').sort().join('')
+//   let sort = (str)=> str.split('').sort().join('')
+//   return sort(s) === sort(t)
+  let map = {};
+   if(s.length !== t.length) {
+     return false;
+   }
 
-  return sort(s) === sort(t)
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) {
+      map[s[i]] += 1;
+    } else {
+      map[s[i]] = 1;
+    }
+  }
+
+  for (let j = 0; j < t.length; j++) {
+    if (!map[t[j]]) {
+      return false;
+    } else {
+      map[t[j]] -= 1;
+    }
+  }
+
+  return true;
 };
