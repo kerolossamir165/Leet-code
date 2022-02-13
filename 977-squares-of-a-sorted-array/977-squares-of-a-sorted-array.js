8dfcbf7366ac2631c5f8a2d1def718756366a849
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-13 15:32:35 +0200

    Time: 408 ms (5.03%), Space: 55 MB (5.63%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -4,5 +4,11 @@
  */
 let getPower = (num) => Math.pow(num, 2);
 var sortedSquares = function(nums) {
-  return nums.map((el) => getPower(el)).sort((a, b) => a - b);  
+   let newNums = [];
+  for (let i = 0; i < nums.length; i++) {
+    console.log(Math.pow(nums[i], 2));
+    newNums.push(getPower(nums[i]));
+  }
+
+  return newNums.sort((a, b) => a - b);
 };
