8797b68d6b95921a6c98e239bc184ded7524a539
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-13 16:26:51 +0200

    Time: 110 ms (70.05%), Space: 49.4 MB (21.66%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,10 +3,12 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let flag = 0;
-  for (let i = 0; i < s.length; i++) {
-    s.splice(i, 0, s.pop());
-    flag++;
-  }
-  return s;
+  //   let flag = 0;
+  // for (let i = 0; i < s.length; i++) {
+  //   s.splice(i, 0, s.pop());
+  //   flag++;
+  // }
+  // return s;
+    
+return s.reverse()
 };
