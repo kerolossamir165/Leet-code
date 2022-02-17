c04572611b7b22de0519eb960770e1ebb5362ac4
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-17 23:32:06 +0200

    Time: 106 ms (54.98%), Space: 45 MB (34.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,16 +2,26 @@
  * @param {string} s
  * @return {boolean}
  */
-let removeSpace = (s, reverse = false) => {
-  let text = s
-    .replace(/[\s`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "")
-    .split("");
+// let removeSpace = (s, reverse = false) => {
+//   let text = s
+//     .replace(/[\s`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "")
+//     .split("");
 
-  return reverse
-    ? text.reverse().join("").toLowerCase()
-    : text.join("").toLowerCase();
-};
+//   return reverse
+//     ? text.reverse().join("").toLowerCase()
+//     : text.join("").toLowerCase();
+// };
 
 var isPalindrome = function (s) {
-  return removeSpace(s, true) === removeSpace(s);
+  // return removeSpace(s, true) === removeSpace(s);
+  let text = s
+    .toLowerCase()
+    .replace(/[\s`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== text[text.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 };
