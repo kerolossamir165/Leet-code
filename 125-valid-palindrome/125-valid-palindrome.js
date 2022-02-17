1a8edcdedc8c3d8e82193b0df9dc91fa76204843
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-17 23:32:50 +0200

    Time: 105 ms (55.54%), Space: 44.4 MB (42.97%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -16,7 +16,8 @@ var isPalindrome = function (s) {
   // return removeSpace(s, true) === removeSpace(s);
   let text = s
     .toLowerCase()
-    .replace(/[\s`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+    // .replace(/[\s`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+    .replace(/\W|_/g, "");
 
   for (let i = 0; i < text.length; i++) {
     if (text[i] !== text[text.length - i - 1]) {
