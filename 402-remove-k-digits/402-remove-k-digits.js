a3af8edaca0cc7dcc3be86025c4f15dad4659c64
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-18 10:59:32 +0200

    Time: 100 ms (56.85%), Space: 44.5 MB (18.27%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    if(num.length===k)return '0';
+    
+    const stack=[];
+    
+    for(let i=0;i<num.length;i++){
+        while(k&&stack.length>0&&stack[stack.length-1]>num[i]){
+            stack.pop()
+            k--
+        }
+        stack.push(num[i])
+    }
+    while(k--){
+        stack.pop()
+    }
+    while(stack[0]==="0"&&stack.length>1){
+        stack.shift()
+    }
+    return stack.join('')
+};
