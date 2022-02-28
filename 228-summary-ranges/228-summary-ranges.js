ab2677a30da260ef9c92382d695fbc1c4c37356d
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-02-28 14:55:04 +0200

    Time: 64 ms (88.02%), Space: 42.5 MB (10.98%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.js
+++ 228-summary-ranges/228-summary-ranges.js
@@ -3,15 +3,18 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
-      let list = [];
+  let list = [];
   for (let i = 0; i < nums.length; i++) {
     let num = nums[i];
     let string = num + "";
-
+      
+    // if the nextIndex === nextValue in array ? include it in range : continue
     while (nums[i + 1] === nums[i] + 1) {
+      // means include it into array 
       i++;
     }
-
+    
+    // check the shape of the string
     if (nums[i] !== num) {
       string += "->" + nums[i];
     }
