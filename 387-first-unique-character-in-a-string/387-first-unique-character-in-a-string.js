11deeeb7f28e7331359206a6a0350fe8bea09956
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-03-02 23:43:59 +0200

    Time: 142 ms (58.61%), Space: 45.8 MB (35.41%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -3,16 +3,7 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-  // let map = {};
-  // for (let i = 0; i < s.length; i++) {
-  //   if (map[s[i]]) {
-  //     map[s[i]] += 1;
-  //   } else {
-  //     map[s[i]] = 1;
-  //   }
-  // }
-  // return s.indexOf(Object.keys(map).find((key) => map[key] === 1));
-      let map = {};
+  let map = {};
   for (let i = 0; i < s.length; i++) {
     if (map[s[i]]) {
       map[s[i]] += 1;
@@ -20,8 +11,6 @@ var firstUniqChar = function(s) {
       map[s[i]] = 1;
     }
   }
-  for(let i = 0 ; i < s.length ; i++ ) {
-    if(map[s[i]] === 1 ) {return i}
-  }
-  return -1
+  return s.indexOf(Object.keys(map).find((key) => map[key] === 1));
+ 
 };
