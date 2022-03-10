14c1f1127c347999d08793555134952a774e75e4
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-03-10 10:15:20 +0200

    Time: 91 ms (34.14%), Space: 42.3 MB (30.49%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.js
+++ 413-arithmetic-slices/413-arithmetic-slices.js
@@ -7,8 +7,9 @@ var numberOfArithmeticSlices = function(nums) {
   let result = 0;
 
   for (let i = 0; i < nums.length; i++) {
-  
-    if (nums[i] - nums[i + 1] ===  nums[i + 1] - nums[i + 2]) {
+    let dif1 = nums[i] - nums[i + 1];
+    let dif2 = nums[i + 1] - nums[i + 2];
+    if (dif1 === dif2) {
       flag++;
       result += flag;
     } else {
