042ebc6350013e8d1af34191740ef64a41ce43d7
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-03-29 11:20:34 +0200

    Time: 171 ms (30.56%), Space: 69.8 MB (5.01%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+ let map = new Map();
+  let position;
+
+  for (let i = 0; i < nums.length; i++) {
+    position = map.get(nums[i]) || 0;
+    map.set(nums[i], position + 1);
+  }
+
+  for (let [key, val] of map) {
+    if (val > 1) return key
+  }
+  return -1;
+    
+};
