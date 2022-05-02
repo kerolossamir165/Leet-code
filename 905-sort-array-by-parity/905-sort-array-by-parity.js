e4ec1bdc7cd9f88c487a5f728d677503697c874a
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-05-02 13:03:12 +0200

    Time: 81 ms (80.49%), Space: 45.1 MB (21.97%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+   let arr = []
+   for(let i = 0 ; i < nums.length ; i++ ) {
+     if(nums[i]%2 === 0 ) {
+       arr.unshift(nums[i])
+     } else {
+       arr.push(nums[i])
+     }
+   }
+
+   return arr
+};
