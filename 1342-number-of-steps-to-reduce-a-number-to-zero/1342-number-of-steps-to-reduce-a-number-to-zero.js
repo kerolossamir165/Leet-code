5433f8929203dd44fc2bf1b16993cd68e2ad9d7c
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-05-27 14:29:57 +0200

    Time: 101 ms (17.06%), Space: 41.8 MB (85.93%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -2,18 +2,35 @@
  * @param {number} num
  * @return {number}
  */
-var numberOfSteps = function(num) {
-      let step = 0;
 
-  while (num > 0) {
-    if (num % 2 === 0) {
-      step++;
-      num = num / 2;
-    } else {
-      step++;
-      num = num - 1;
-    }
+let recFun = (num, step = 0) => {
+  if (num === 0) return step;
+  if (num % 2 === 0) {
+    step++;
+    return recFun(num / 2, step);
+  } else {
+    step++;
+    return recFun(num - 1, step);
   }
+};
 
-  return step;
+
+
+var numberOfSteps = function(num) {
+    
+      return recFun(num)
+
+//       let step = 0;
+
+//   while (num > 0) {
+//     if (num % 2 === 0) {
+//       step++;
+//       num = num / 2;
+//     } else {
+//       step++;
+//       num = num - 1;
+//     }
+//   }
+
+//   return step;
 };
