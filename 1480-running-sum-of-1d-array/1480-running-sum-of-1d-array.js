9d8b11445da23243558e5fda5c6f445c579c9028
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-06-01 14:38:59 +0200

    Time: 75 ms (62.36%), Space: 48.3 MB (5.07%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let newArr = [];
+
+  for (let i = 0; i < nums.length; i++) {
+   
+    newArr.push(nums.slice(0, i + 1).reduce((acc, cur) => acc + cur, 0));
+    
+  }
+  return newArr;
+};
