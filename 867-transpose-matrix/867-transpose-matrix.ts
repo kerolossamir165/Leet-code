b86788ca99bc9d15f063beb506bf4824687af3c6
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-06-02 12:40:18 +0200

    Time: 109 ms (54.55%), Space: 45.7 MB (31.82%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.ts
@@ -0,0 +1,11 @@
+function transpose(matrix: number[][]): number[][] {
+  let arr:number[][] = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    arr.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      arr[arr.length - 1].push(matrix[j][i]);
+    }
+  }
+
+  return arr;
+};
