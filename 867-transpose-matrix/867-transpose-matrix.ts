efe3f9eb52ac89d1b338999ac3d03935227a59aa
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-06-02 13:06:04 +0200

    Time: 81 ms (100.00%), Space: 45.8 MB (31.82%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.ts
+++ 867-transpose-matrix/867-transpose-matrix.ts
@@ -1,7 +1,10 @@
 function transpose(matrix: number[][]): number[][] {
   let arr:number[][] = [];
+  // loop through every element in the first array 
   for (let i = 0; i < matrix[0].length; i++) {
+    
     arr.push([]);
+    // loop through every element in the matrix 
     for (let j = 0; j < matrix.length; j++) {
       arr[arr.length - 1].push(matrix[j][i]);
     }
