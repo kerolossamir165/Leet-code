5d768ea804398c327c40ea56341cf13b5a48e7fa
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-06-12 22:44:30 +0200

    Time: 95 ms (37.56%), Space: 45 MB (70.51%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,5 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target);
+    // return nums.indexOf(target);
+  let left = 0 
+  let right = nums.length - 1 
+
+  while(left <= right) {
+    let middle = Math.floor((left + right) / 2)
+   
+    if(nums[middle] === target ) {
+       return middle 
+    } else if (nums[middle] < target ) {
+      left = middle + 1 
+    } else {
+      right = middle - 1
+    }
+  }
+  return -1
 };
