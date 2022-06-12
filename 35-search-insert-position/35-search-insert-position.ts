cbb69c68aa5c0c8d81fbb85b8df27301a7921fb8
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-06-12 23:27:21 +0200

    Time: 102 ms (30.34%), Space: 44.8 MB (29.62%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.ts
@@ -0,0 +1,16 @@
+function searchInsert(nums: number[], target: number): number {
+    let start: number = 0 
+    let end : number  =  nums.length - 1
+    while(start <= end ) {
+        let middle: number = Math.floor((start + end ) / 2 )
+        
+        if(target === nums[middle]) return middle 
+        else if( target < nums[middle ]) {
+            end = middle - 1 
+        } else {
+            start = middle + 1 
+        }
+        
+    }
+    return start
+};
