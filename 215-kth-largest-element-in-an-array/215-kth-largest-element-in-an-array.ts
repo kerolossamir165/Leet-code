a2a4ffd00b47023fb9067a5fd68f81fdfd0b9b08
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-06-22 15:29:57 +0200

    Time: 133 ms (38.97%), Space: 44.5 MB (89.71%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.ts
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.ts
@@ -1,15 +1,15 @@
 function findKthLargest(nums: number[], k: number): number {
- let done = false;
-  while (!done) {
-    done = true;
-    for (var i = 1; i < nums.length; i += 1) {
-      if (nums[i - 1] > nums[i]) {
-        done = false;
-        let tmp = nums[i - 1];
-        nums[i - 1] = nums[i];
-        nums[i] = tmp;
-      }
-    }
-  }
-  return nums[nums.length - k]
+ // let done = false;
+ //  while (!done) {
+ //    done = true;
+ //    for (var i = 1; i < nums.length; i += 1) {
+ //      if (nums[i - 1] > nums[i]) {
+ //        done = false;
+ //        let tmp = nums[i - 1];
+ //        nums[i - 1] = nums[i];
+ //        nums[i] = tmp;
+ //      }
+ //    }
+ //  }
+  return nums.sort((a,b)=> a - b)[nums.length - k]
 };
