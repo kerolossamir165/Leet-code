9aab98e3a5da921117794d1ba01ae867ba9862f6
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-07-06 16:00:10 +0200

    Time: 441 ms (5.01%), Space: 48.1 MB (5.27%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -8,11 +8,16 @@ var fib = function(n) {
     // }
     // return fib(n - 1) + fib(n - 2)
     
-   let map = {
-   0 : 0 ,
-   1 : 1 
- }
+//    let map = {
+//    0 : 0 ,
+//    1 : 1 
+// }
   
-  if (!n || map[n]) return map[n];
-  return map[n] = fib(n - 1) + fib(n - 2);
+//   if (!n || map[n]) return map[n];
+//   return map[n] = fib(n - 1) + fib(n - 2);
+    
+  let memo = {0 : 0 , 1 : 1};
+  if (!n || memo[n]) return memo[n];
+  memo[n] = fib(n - 1) + fib(n - 2);
+  return memo[n];
 };
