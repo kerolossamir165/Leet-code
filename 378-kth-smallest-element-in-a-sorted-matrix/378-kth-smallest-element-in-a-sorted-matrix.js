41c80d10bc90d11e65aa6ce2efa390a5566eb9ef
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2022-08-02 15:59:45 +0200

    Time: 156 ms (40.58%), Space: 47.6 MB (63.69%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.js
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.js
@@ -5,7 +5,6 @@
  */
 var kthSmallest = function(matrix, k) {
     let newMaxtix = matrix.flat().sort((a , b )=> a - b )
-  
     return newMaxtix.find((el , i) => {
       if(i + 1 === k ) {
         return el
