84c0dd89d51f75ae8e81d22f5a5926500a5f5bb6
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2024-09-03 12:44:04 +0300

    Time: 53 ms (74.67%), Space: 50.4 MB (30.36%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.js
+++ 0682-baseball-game/0682-baseball-game.js
@@ -3,11 +3,75 @@
  * @return {number}
 */
 
-var calPoints = function(operations) {
-    let stack = []
-    let sum = 0;
-    for(let i = 0; i < operations.length; i++) {
-        switch(operations[i]) { 
+// var calPoints = function(operations) {
+//     let stack = []
+//     let sum = 0;
+//     for(let i = 0; i < operations.length; i++) {
+//         switch(operations[i]) { 
+//             case 'C':
+//                 stack.pop()
+//                 break;
+//             case 'D':
+//                 let top = stack.pop() 
+//                 stack.push(top)
+//                 stack.push(top * 2);
+//                 break;
+//             case '+':
+//                 let top1 = stack.pop();
+//                 let top2 = stack.pop();
+//                 stack.push(top2);
+//                 stack.push(top1);
+//                 stack.push(top1 + top2);
+                        
+//                  break;
+//             default:
+//                  stack.push(parseInt(operations[i]));
+//             }
+//      }
+
+          
+//     for (let element of stack) {
+//         sum += element
+//     }
+//     return sum;
+    
+// };
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.head = null
+    }
+
+    push(value) {
+        let newNode = new Node(value)
+        if(!this.head) {
+            this.head = newNode
+            return
+        }
+        newNode.next = this.head
+        this.head = newNode
+    }
+
+    pop() {
+        if(!this.head) return null
+        let poppedValue = this.head.value
+        this.head = this.head.next
+        return poppedValue
+    }
+}
+let calPoints = (operations)=> {
+    let stack = new Stack()
+    let score = 0
+
+    for (let i = 0 ; i < operations.length; i++) {
+        switch (operations[i]) { 
             case 'C':
                 stack.pop()
                 break;
@@ -27,12 +91,10 @@ var calPoints = function(operations) {
             default:
                  stack.push(parseInt(operations[i]));
             }
-     }
+        }
 
-          
-    for (let element of stack) {
-        sum += element
-    }
-    return sum;
-    
-};
+        while (stack.head) {
+            score += stack.pop()
+        }
+        return score
+}
