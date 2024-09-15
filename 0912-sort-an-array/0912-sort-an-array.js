24460d9dd0ab08cc6565d6564da436a29b82af82
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2024-09-16 01:10:55 +0300

    Time: 1443 ms (5.12%), Space: 72.5 MB (45.49%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,48 +2,86 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-function mergeSort(arr) {
+// function mergeSort(arr) {
 
-    if(arr.length === 1) return arr
+//     if(arr.length === 1) return arr
 
-    let mid = Math.floor(arr.length  /2 )
+//     let mid = Math.floor(arr.length  /2 )
 
-    let left = arr.slice(0, mid)
-    let right = arr.slice(mid)
+//     let left = arr.slice(0, mid)
+//     let right = arr.slice(mid)
 
-    let leftSorted = mergeSort(left)
-    let rightSorted = mergeSort(right)
+//     let leftSorted = mergeSort(left)
+//     let rightSorted = mergeSort(right)
 
-    return merge(leftSorted, rightSorted)
+//     return merge(leftSorted, rightSorted)
 
+// }
+
+// function merge(left, right) {
+//     let temp = []
+//     let leftPointer = 0
+//     let rightPointer = 0
+
+//     while(leftPointer < left.length && rightPointer < right.length) {
+//         if( left[leftPointer] < right[rightPointer]) {
+//             temp.push(left[leftPointer])
+//             leftPointer++
+//         }else {
+//             temp.push(right[rightPointer])
+//             rightPointer++;
+//         }
+//     }
+
+//     while(leftPointer < left.length){
+//         temp.push(left[leftPointer])
+//         leftPointer++
+//     }
+
+//     while(rightPointer < right.length){
+//         temp.push(right[rightPointer])
+//         rightPointer++
+//     }
+//     return temp
+// }
+
+function merge(arr, low , mid , high) {
+    let aux = []
+    for(let k = low ; k <= high ; k++) {
+        aux[k] = arr[k]
+    }
+    let i = low
+    let j = mid + 1
+
+
+    for (let k = low ; k <= high; k++) {
+        if(i > mid) {
+            arr[k] = aux[j++]
+        }else if (j > high) {
+            arr[k] = aux[i++]
+        }else if (aux[i] <= aux[j]) {
+            arr[k]= aux[i++]
+        }else {
+            arr[k] = aux[j++]
+        }
+    }  
+    return arr
 }
 
-function merge(left, right) {
-    let temp = []
-    let leftPointer = 0
-    let rightPointer = 0
 
-    while(leftPointer < left.length && rightPointer < right.length) {
-        if( left[leftPointer] < right[rightPointer]) {
-            temp.push(left[leftPointer])
-            leftPointer++
-        }else {
-            temp.push(right[rightPointer])
-            rightPointer++;
+function mergeSort(arr, low = 0, high= arr.length - 1) {
+   
+    let N = arr.length
+    for(let size = 1 ; size < N ; size += size) {
+        for(let low = 0; low <  N - size ; low += 2*size) {
+            let mid = low + size - 1
+            let high = Math.min(low + 2*size - 1, N - 1)
+            merge(arr, low, mid, high)
         }
     }
-
-    while(leftPointer < left.length){
-        temp.push(left[leftPointer])
-        leftPointer++
-    }
-
-    while(rightPointer < right.length){
-        temp.push(right[rightPointer])
-        rightPointer++
-    }
-    return temp
+    return arr
 }
+
 
 
 var sortArray = function(nums) {
